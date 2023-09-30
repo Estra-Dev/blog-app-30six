f2619e22be254d996caca34c3000d05b5a637bbb
Estra-Dev <dominikolyson@gmail.com>
2023-09-30 12:22:36 +0100

    arranged First Letters

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './pages/Home'
-//import Login from './pages/login.jsx'
-// import Register from './pages/register.jsx'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import Layout from './Layout'
 import { UserContextProvider } from './UserContext'
 import AddEdit from './pages/AddEdit'
@@ -19,8 +19,8 @@ function App() {
             <Routes>
               <Route path='/' element={<Layout />}>
                 <Route index element={<Home />} />
-                {/* <Route path='/login' element={<Login />} /> */}
-                {/* <Route path='/register' element={<Register />} /> */}
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
                 <Route path='/addpost' element={<AddEdit />} />
                 <Route path='/post/:id' element={<SinglePost />} />
                 <Route path='/edit/:id' element={<EditPost />} />
